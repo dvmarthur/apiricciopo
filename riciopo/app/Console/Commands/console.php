d4608602064e9fe0e16e41715e9d33d074c4653a
Arthur Dias <arthurdvm2@gmail.com>
2023-11-25 14:32:32 -0300

    att

--- riciopo/app/Console/Commands/console.php
+++ riciopo/app/Console/Commands/console.php
@@ -30,37 +30,38 @@ class console extends Command
     {
 
 
-        // Caminho para o arquivo CSV
-        $csvFilePath = 'planilha/catalogo_isapaB2B (1).csv';
+       // Caminho para o arquivo TSV
+$tsvFilePath = 'planilha/planilha.tsv';
 
-        // Abre o arquivo CSV para leitura
-        $file = fopen($csvFilePath, 'r');
+// Abre o arquivo TSV para leitura
+$file = fopen($tsvFilePath, 'r');
 
-        // Verifica se o arquivo foi aberto com sucesso
-        if ($file) {
-            // Loop para ler cada linha do arquivo
-            while (($data = fgetcsv($file, null, ',')) !== FALSE) {
+// Verifica se o arquivo foi aberto com sucesso
+if ($file) {
+    // Lê a primeira linha que geralmente contém os cabeçalhos das colunas
+    $headers = fgetcsv($file, 1000, "\t");
 
+    // Índices das colunas desejadas
+    $columnsToPrint = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
-               // die(var_dump($data));
-                // $data é um array contendo os dados da linha atual
+    // Loop para ler cada linha do arquivo
+    while (($data = fgetcsv($file, 1000, "\t")) !== FALSE) {
+        // $data é um array contendo os dados da linha atual
 
-                // Aqui, você pode realizar operações com os dados, se necessário
-                // Neste exemplo, vamos apenas imprimir os dados na tela
-                echo "Sku: " . $data[0] . "\n";
-                echo "Descrição: " . $data[1] . "\n";
-                echo "Sessão: " . $data[2] . "\n";
-                // ... adicione mais campos conforme necessário
-
-               // echo "<hr>"; // Adiciona uma linha horizontal para separar os registros
-            }
-
-            // Fecha o arquivo após a leitura
-            fclose($file);
-        } else {
-            // Se o arquivo não puder ser aberto, exibe uma mensagem de erro
-            echo "Erro ao abrir o arquivo CSV.";
+        // Itera sobre os índices das colunas desejadas e imprime as informações
+        foreach ($columnsToPrint as $index) {
+            echo "$headers[$index]: $data[$index]\n";
         }
+
+       // echo "<hr>"; // Adiciona uma linha horizontal para separar os registros
+    }
+
+    // Fecha o arquivo após a leitura
+    fclose($file);
+} else {
+    // Se o arquivo não puder ser aberto, exibe uma mensagem de erro
+    echo "Erro ao abrir o arquivo TSV.";
+}
 
 
         return Command::SUCCESS;
